8a53b44cf4ecd1981cf6ec974b5fcaa072b5fdd7
brownviper <s4saz@hotmail.com>
2024-05-27 12:30:02 +0100

    main: processing is started from the last element

--- src/skyscraper_util.py
+++ src/skyscraper_util.py
@@ -1,27 +1,19 @@
-def get_max_skyscraper_height(skyscraper_heights):
-    max_skyscraper_height = 0
-    max_skyscraper_height_index = 0
-    index = 0
-    for skyscraper_height in skyscraper_heights:
-        if skyscraper_height >= max_skyscraper_height:
-            max_skyscraper_height = skyscraper_height
-            max_skyscraper_height_index = index
-        index = index + 1
-
-    return max_skyscraper_height, max_skyscraper_height_index
-
-
-def process_skyscrapers_heights(skyscraper_heights):
-    results = []
-
-    for height in skyscraper_heights:
-        if number_is_unique(height, results):
-            results.append(height)
-
-    return results
-
-
-def number_is_unique(number, skyscraper_heights):
+def number_exist_in_array(number, skyscraper_heights):
     idx = len(list(filter(lambda x: x == number, skyscraper_heights)))
 
-    return idx == 1
+    return idx > 0
+
+
+def process_skyscrapers_heights(heights):
+    results = [heights[-1]]
+
+    for height in reversed(heights[:-1]):
+        if number_exist_in_array(height, results) is False:
+            results.insert(0, height)
+        else:
+            tmp = height - 1
+            while number_exist_in_array(tmp, results) is True:
+                tmp = tmp-1
+            results.insert(0, tmp)
+
+    return results
--- src/tests/test_sky_scraper_util.py
+++ src/tests/test_sky_scraper_util.py
@@ -1,19 +1,31 @@
 # src/tests/test_sky_scraper_util.py
 
-from src.skyscraper_util import process_skyscrapers_heights, number_is_unique
+from src.skyscraper_util import process_skyscrapers_heights, number_exist_in_array
 
 
-def test_number_is_unique_return_true():
+def test_number_exist_in_array_returns_true():
     heights = [4, 9, 1]
-    assert number_is_unique(4, heights) == True
+    assert number_exist_in_array(4, heights) == True
 
 
-def test_number_is_unique_return_false():
-    heights = [4, 9, 4]
-    assert number_is_unique(4, heights) == False
+def test_number_exist_in_array_returns_false():
+    heights = [9, 4]
+    assert number_exist_in_array(5, heights) == False
 
 
-def test_max_skyscraper_is_kept():
-    skyScrapersHeights = [4, 9]
+def test_skyscraper_are_processed_for_unique_heights():
+    heights = [4, 9]
 
-    assert process_skyscrapers_heights(skyScrapersHeights) == [4, 9]
+    assert process_skyscrapers_heights(heights) == [4, 9]
+
+
+def test_heights_are_interpolated_for_duplicated_heights():
+    heights = [4, 4, 9]
+
+    assert process_skyscrapers_heights(heights) == [3, 4, 9]
+
+
+def test_processing_is_starting_from_last_element():
+    heights = [2, 5, 4, 5, 5]
+
+    assert process_skyscrapers_heights(heights) == [1, 2, 3, 4, 5]
